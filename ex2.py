43525de23c2a304a83e96aca66a35eadf1abbfd0
lillmao69 <157451782+lillmao69@users.noreply.github.com>
2024-04-08 11:10:44 +0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# Запрос чисел у пользователя
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+
+# Вычисление суммы и произведения чисел
+sum_result = num1 + num2
+multiply_result = num1 * num2
+
+# Вывод результатов
+print("Результат сложения:", sum_result)
+print("Результат умножения:", multiply_result)
